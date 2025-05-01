a6997f2615c9dd962c081b2a1be085d9e8c97ce5
Dheeraj Adhikri <adhkaridheeraj007@gmail.com>
2025-05-01 19:12:36 +0530

    adding icons to delete and add button

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState();
-  const [dueDate, setDueDate] = useState();
+  const [todoName, setTodoName] = useState([]);
+  const [dueDate, setDueDate] = useState([]);
 
   const handleNameChange = (event) => {
     setTodoName(event.target.value);
@@ -38,7 +39,7 @@ function AddTodo({ onNewItem }) {
             className="btn btn-success kg-button"
             onClick={handleAddButtonClicked}
           >
-            Add
+            <IoIosAddCircleOutline />
           </button>
         </div>
       </div>
